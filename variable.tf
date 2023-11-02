1df103049ab63cfde2b4f2f298de1917ccfb139f
keepinit <mayankr1915@gmail.com>
2023-11-02 12:43:09 +0530

    made changes

--- variable.tf
+++ variable.tf
@@ -11,7 +11,7 @@ variable "instance_type" {
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  default     = "my-ec2"
 }
 
 variable "ami_id" {
